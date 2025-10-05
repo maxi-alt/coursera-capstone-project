971f8dff779cb5183d552d775852765f97057f45
maxim <maxim@kozik.it>
2025-10-05 14:57:12 +0200

    meta tags and semantic html structure

---
+++ little-lemon/src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="/logo.png" alt="Little Lemon logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ little-lemon/src/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reserve">Reserve</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
